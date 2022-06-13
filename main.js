44b43a19b6e66ba7ade8f24c6bf02c174c4c3e54
PiriyaphongW <piriyaphong1640@gmail.com>
2022-06-13 18:23:55 +0700

    third commit

--- main.js
+++ main.js
@@ -1,40 +1,67 @@
 function toggle(open) {
-    const toggleClose = document.getElementById('nav-open')
-    toggleClose.classList.remove('active-mobile')
+  const toggleClose = document.getElementById('nav-open')
+  toggleClose.classList.remove('active-mobile')
 
-    const toggleCloseMain = document.getElementById('nav-main')
-    toggleCloseMain.classList.add('d-none')
+  const toggleCloseMain = document.getElementById('nav-main')
+  toggleCloseMain.classList.add('d-none')
 }
 
 function toggleHidden(close) {
-    const toggleCloseMain = document.getElementById('nav-main')
-    toggleCloseMain.classList.remove('d-none')
-    const toggleClose = document.getElementById('nav-open')
-    toggleClose.classList.add('active-mobile')
+  const toggleCloseMain = document.getElementById('nav-main')
+  toggleCloseMain.classList.remove('d-none')
+  const toggleClose = document.getElementById('nav-open')
+  toggleClose.classList.add('active-mobile')
 }
 
 
 
 $('.owl-carousel-first').owlCarousel({
-  items:1,
-  loop:true,
-  merge:true,
-  nav:true,
-  navText:["<i class='fa-solid fa-angle-left'></i>","<i class='fa-solid fa-angle-right'></i>"],
+  items: 1,
+  loop: true,
+  merge: true,
+  nav: true,
+  navText: ["<i class='fa-solid fa-angle-left'></i>", "<i class='fa-solid fa-angle-right'></i>"],
 
 });
 
 
 $('.owl-carousel-second').owlCarousel({
-  items:5,
-  loop:true,
-  dots:false,
-  nav:true,
- 
+  items: 1,
+  loop: true,
+  dots: false,
+  nav: true,
+  navText: ["<i class='fa-solid fa-angle-left'></i>", "<i class='fa-solid fa-angle-right'></i>"],
+  responsive: {
+    576: {
+      items: 5,
+    },
+
+  }
 
 });
- 
- 
+
+
+$('.owl-carousel-third').owlCarousel({
+  items: 1,
+  merge: true,
+  loop: true,
+  video: true,
+  lazyLoad:true,
+  center: true,
+  videoWidth: 400,
+  videoHeight: 400,
+
+  responsive: {
+    480: {
+      items: 2
+    },
+    600: {
+      items: 4
+    }
+  }
+})
+
+
 
 
 
